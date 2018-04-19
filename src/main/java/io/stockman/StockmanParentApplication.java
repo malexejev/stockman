d55e90215a49fc47ede39455015fed31bbf85dd3
Max <maksim.alekseev@alibaba-inc.com>
2018-04-20 00:02:53 +0300

    Kickoff: spring boot 2, java 10, lombok.

---
+++ src/main/java/io/stockman/StockmanParentApplication.java
@@ -0,0 +1,12 @@
+package io.stockman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockmanParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockmanParentApplication.class, args);
+	}
+}
---
+++ src/test/java/io/stockman/StockmanParentApplicationTests.java
@@ -0,0 +1,16 @@
+package io.stockman;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StockmanParentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
